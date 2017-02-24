c2f5e108687f1c9d4bc7f1cb9f7bb3b3307bc1a0
yirosy <yirosy@qq.com>
2017-02-24 21:31:24 +0800

    Make window and background panel transparent.

--- src/main/java/com/github/tamcl/gui/BackgroundPane.java
+++ src/main/java/com/github/tamcl/gui/BackgroundPane.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 /**
  * Created by I331709 on 2/23/2017.
+ * Background pane
  */
 public class BackgroundPane extends JLayeredPane {
 
@@ -14,16 +15,16 @@ public class BackgroundPane extends JLayeredPane {
     }
 
     private void initBackgroundPane () {
-        this.setBounds(0 , 0, 200, 200);
+        this.setBounds(0 , 0, 1200, 1000);
         this.setLayout(null);
         JPanel bgPanel = new JPanel();
-        bgPanel.setBounds(0, 0, 200, 200);
+        bgPanel.setBounds(200, 200, 900, 500);
+        bgPanel.setOpaque(false);
         this.add(bgPanel);
+
         JLabel bgLabel = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(
                 getClass().getResource("/images/bkg.png"))));
-        bgLabel.setSize(200,200);
-        bgLabel.setBackground(Color.black);
-        //bgPanel.setBackground(Color.black);
+        bgPanel.setBorder(null);
         bgLabel.setVisible(true);
         bgPanel.add(bgLabel);
         this.setBackground(Color.black);
--- src/main/java/com/github/tamcl/gui/MainWindow.java
+++ src/main/java/com/github/tamcl/gui/MainWindow.java
@@ -1,9 +1,12 @@
 package com.github.tamcl.gui;
 
+import com.sun.awt.AWTUtilities;
+
 import javax.swing.*;
 
 /**
  * Created by I331709 on 2/22/2017.
+ * Main window JFrame
  */
 public class MainWindow extends JFrame{
     public MainWindow () {
@@ -12,9 +15,10 @@ public class MainWindow extends JFrame{
 
     private void initWindow () {
         this.setTitle("test");
-        this.setBounds(50, 50, 200, 200);
+        this.setBounds(0, 0, 1200, 1000);
         this.setLayout(null);
         this.setUndecorated(true);
+        AWTUtilities.setWindowOpaque(this, false);
     }
 
 }
